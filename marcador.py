d652f9b08d7a1a1317f65997aa12095346645a1f
luisa0525 <142631554+luisa0525@users.noreply.github.com>
2023-12-07 18:27:23 -0500

    cuarto

---
+++ marcador.py
@@ -0,0 +1,27 @@
+from db import db
+
+class marcador(db.Model):
+
+    # nombre
+
+    __tablename__="marcador"
+
+    # conjunto de atributos
+
+    id=db.Column(db.Integer, primary_key=True)
+    
+    nombre=db.Column(db.string(50))
+    latitud=db.Column(db.Float(50))
+    longitud=db.Column(db.Float(50))
+    direccion=db.Column(db.String(100))
+    contacto=db.Column(db.string(10))
+
+    # metodo constructor
+
+    def __init__(self, nombre, latitud, longitud, direccion, contacto):
+
+        self.latitud=latitud
+        self.latitud=latitud
+        self.longitud=longitud
+        self.direccion=direccion
+        self.contacto=contacto
---
+++ programa.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template, request, redirect, url_for
+from db import db
+from marcador import marcador
+
+class programa:
+
+    def __init__(self):
+
+        self.app=Flask(__name__)
+        self.app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///marcadores.sqlite3"
+
+        db.init_app(self.app)
+
+        self.app.add_url_rule('/', view_func=self.Visor)
+        self.app.add_url_rule('/nuevo', view_func=self.agregar, methods=["GET","POST"])
+        self.app.add_url_rule('/puntos', view_func=self.puntitos)
+
+        #iniciar el servidor
+        with self.app.app_context():
+            db.create_all()
+            self.app.run(debug=True)
+
+    def Visor(self):
+        return render_template('Visor.html', Marcador=marcador.query.all())
+    def puntitos(self):
+        return render_template('mostrarTodos.html', Marcador=marcador.query.all())
+
+    def agregar(self):
+
+        #VERIFICAR OPERACION
+
+        if request.method=="POST":
+
+            #CREAR OBJETO
+            nombre=request.form['nombre']
+            latitud=request.form['latitud']
+            longitud=request.form['longitud']
+            direccion=request.form['descripcion']
+            contacto=request.form['contacto']
+            miMarcador=marcador(latitud, longitud, direccion, contacto)
+
+            #guardar el objeto
+            db.session.add(miMarcador)
+            db.session.commit()
+
+            return redirect(url_for('Visor'))
+
+
+
+        return render_template('nuevomarcador.html')
+    
+miPrograma=programa()
